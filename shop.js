cc6618b8aa6a1011253a8c35c013419b17e35a75
Abdulgani3123 <146915096+Abdulgani3123@users.noreply.github.com>
2024-12-17 11:22:57 +0530

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,10 @@
+let cartBtn=document.getElementById('btnShop');
+for(let i=0;i<cartBtn.length;i++){
+  cartBtn[i].addEventListener('click',addToCart);
+}
+
+function addToCart(event){
+  btn=event.target
+  console.log(btn);
+}
+
